47058044be6e45860f0c42305294d82259b0798a
Hassan Aw <awale822@gmail.com>
2023-12-27 17:36:03 +0300

    lesson three

---
+++ script.js
@@ -0,0 +1,23 @@
+
+myName = "Hassan awale"
+
+console.log(myName);
+
+var a;
+var b = 2;
+a = b;
+console.log(b);
+
+// initialization of variables the below code is uninitializes 
+var d;
+var e;
+var f;
+// initialization of variables  
+var d = 10;
+var e = 11;
+var f = "string";
+
+// case sensitivity 
+
+
+

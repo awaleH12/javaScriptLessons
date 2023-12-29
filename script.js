8d56c47d96e0914a2dcec52d82d055f6c8879ce4
Hassan Aw <awale822@gmail.com>
2023-12-29 10:48:35 +0300

    these two functions if string contains only numbers or letters

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-
-myName = "Hassan awale"
-
 console.log("Welcome to EVC!");
 
 // pay bill
 // nine digit format
-phone = "612101520";
 
-if (phone.length == 9) {
-    if (phone.charAt(0) == 6 || phone.charAt(0) == 7) {
-        if (phone.charAt(1) == 1 || phone.charAt(1) == 2 || phone.charAt(1) == 7) {
-            console.log("correct format");
-        } else {
-            console.log("wrong format");
-        }
-    } else {
-        console.log("correct format");
-    }
-} else {
-    console.log("wrong format");
+
+// creating an check function
+function checkPhone(numbers) {
+  // Use a regular expression to check if the string contains only numbers
+  return /^[0-9]+$/.test(numbers);
 }
 
-// ten digit format
-phone = "0612101520";
-
-if (phone.length == 10) {
-    if (phone.charAt(0) == 0 && phone.charAt(1) == 6) {
-        if (phone.charAt(2) == 1 || phone.charAt(2) == 2) {
-            console.log("correct format");
-        } else {
-            console.log("wrong format");
-        }
-    } else {
-            console.log("wrong format");
-        }
+// Example usage:
+var phone = "612102010";
+if (checkPhone(phone)) {
+  console.log("The string contains only numbers.");
 } else {
-            console.log("wrong format");
-        }
+  console.log("The string contains non-numeric characters.");
+}
+
+// creating an check function
+function checkLetters(letters) {
+  // Use a regular expression to check if the string contains only numbers
+  return /^[a-z]+$/.test(letters);
+}
+
+// Example usage:
+var alpha = "hassan1";
+if (checkLetters(alpha)) {
+  console.log("The string contains only letters.");
+} else {
+  console.log("The string contains non-alphabetic characters.");
+}
